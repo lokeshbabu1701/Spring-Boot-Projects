c6dc00135d0e78306c37dbb2ca3c6c274ba1c1fa
lokeshbabu1701 <lokeshbabu1701@gmail.com>
2020-09-28 02:36:12 +0530

    refs #1 initial commit

---
+++ spring-pet-clinic-app/src/main/java/com/pet/clinic/SpringPetClinicAppApplication.java
@@ -0,0 +1,13 @@
+package com.pet.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetClinicAppApplication.class, args);
+	}
+
+}
---
+++ spring-pet-clinic-app/src/test/java/com/pet/clinic/SpringPetClinicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
